faa28b5c194a537a794eb44bc57c361fa3d1c7d4
welcome <welcome@MADHAN>
2021-09-04 06:50:47 -0700

    Commit

---
+++ Array/src/com/array/Test.java
@@ -0,0 +1,11 @@
+package com.array;
+
+public class Test {
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+    EmployeeServices util=new EmployeeServices();
+    EmployeeDTO arr[]=util.getUserDetails();
+    util.getEmployeeDetails(arr);
+	}
+
+}
